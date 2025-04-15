d71845026878964f91502e1d7afbe9a69c735e01
Sai Kumar <54095747+saikumarv3@users.noreply.github.com>
2025-04-15 16:17:07 -0500

    setup

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,13 +1,4 @@
-import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
-import { TestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-
-setupZoneTestEnv();
-
-TestBed.initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
-);
+import 'jest-preset-angular/setup-jest';
 
 Object.defineProperty(window, 'CSS', { value: null });
 Object.defineProperty(window, 'getComputedStyle', {
